03786d79f8d91ae95c0c9965dc80eb2dc40f8302
jjaemun <jjaemuuu@gmail.com>
2026-09-23 15:38:55 +0200

    rng

--- include/rng/rng.hpp
+++ include/rng/rng.hpp
@@ -2,6 +2,17 @@
 
 
 #include <concepts>
-#include <types>
+#include <span>
 
 #include "rng/types.hpp"
+
+
+namespace rng {
+    template <typename R>
+    concept Rng = requires (R& gen, std::span<std::byte> dst) {
+        { gen.next_u32() } -> std::same_as<u32>;
+        { gen.next_u64() } -> std::same_as<u64>;
+        { gen.fill_bytes(dst) } -> std::same_as<void>;
+    };
+
+} // namespace rng
