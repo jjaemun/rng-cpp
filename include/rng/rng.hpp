aebd4e78b5c99efe30298b872b3c6e01b59e4bb3
jjaemun <jjaemuuu@gmail.com>
2026-09-24 10:06:47 +0200

    docs

--- include/rng/rng.hpp
+++ include/rng/rng.hpp
@@ -8,11 +8,17 @@
 
 
 namespace rng {
+    // Models base concept for random bit generators.  
+    //
+    // Generators satisfying `Rng` should be deterministic sources 
+    // of uniform random bits.
     template <typename R>
     concept Rng = requires (R& gen, std::span<std::byte> dst) {
+        // Generates the next random `u32`.
         { gen.next_u32() } -> std::same_as<u32>;
+        // Generates the next random `u64`.
         { gen.next_u64() } -> std::same_as<u64>;
+        // Fills caller-owned byte buffers. It **should** be allocation free.
         { gen.fill_bytes(dst) } -> std::same_as<void>;
     };
-
 } // namespace rng
