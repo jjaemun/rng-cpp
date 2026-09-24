7560a0986a89765c9b36c81fc583041c55fd511d
jjaemun <jjaemuuu@gmail.com>
2026-09-24 10:34:29 +0200

    docs

--- include/rng/rng.hpp
+++ include/rng/rng.hpp
@@ -16,9 +16,13 @@ namespace rng {
     concept Rng = requires (R& gen, std::span<std::byte> dst) {
         // Generates the next random `u32`.
         { gen.next_u32() } -> std::same_as<u32>;
+
         // Generates the next random `u64`.
         { gen.next_u64() } -> std::same_as<u64>;
-        // Fills caller-owned byte buffers. It **should** be allocation free.
+
+        // Fills caller-owned byte buffer. 
+        //
+        // It provides an allocation-free api for random number generation.
         { gen.fill_bytes(dst) } -> std::same_as<void>;
     };
 } // namespace rng
