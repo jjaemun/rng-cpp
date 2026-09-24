d61be050278c436f342e1550adcd47e67d220415
jjaemun <jjaemuuu@gmail.com>
2026-09-24 18:44:00 +0200

    distribution

---
+++ include/rng/dist/distribution.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+
+#include <concepts>
+#include "rng/rng.hpp"
+
+
+namespace rng::dist {
+    // Return type produced by sampling a distribution `D` endowed with some generator `R`.
+    template <typename D, typename R>
+        requires (Rng<R>)
+    using Sample = decltype(std::declval<D&>().sample(std::declval<&R>()));
+
+    // Models a statistical distribution.
+    template <typename D, typename R>
+        requires (Rng<R>)
+    concept Distribution = requires (D& distr, R& gen, 
+                                               std::span<Sample<D, R> dst)  {
+        // Samples a single element from the given distribution.
+        { distr.sample(gen) } -> std::same_as<Sample<D, R>>;
+        // Fills a contiguous buffer, `dist`, with random samples from the given distribution.
+        { distr.fill(gen, dst) } -> std::same_as<void>;
+    };
+} // namespace rng::dist
