8ad29dbdd52a5f1f68d2bf795cd61545c185c195
jjaemun <jjaemuuu@gmail.com>
2026-09-24 18:45:10 +0200

    distribution

--- include/rng/dist/distribution.hpp
+++ include/rng/dist/distribution.hpp
@@ -15,7 +15,7 @@ namespace rng::dist {
     template <typename D, typename R>
         requires (Rng<R>)
     concept Distribution = requires (D& distr, R& gen, 
-                                               std::span<Sample<D, R> dst)  {
+                                     std::span<Sample<D, R> dst)  {
         // Samples a single element from the given distribution.
         { distr.sample(gen) } -> std::same_as<Sample<D, R>>;
         // Fills a contiguous buffer, `dist`, with random samples from the given distribution.
